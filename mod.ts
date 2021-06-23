7700d47f3ffe50a2e5992bb408dd8884cccb94a4
wioenena-q <brs_kprl09@outlook.com>
2021-06-23 17:41:35 +0300

    Exported 'StringUtils.ts'.

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from "./src/Array/ArrayUtils.ts";
 export * from "./src/Object/ObjectUtils.ts";
 export * from "./src/Function/FunctionUtils.ts";
+export * from "./src/String/StringUtils.ts";
