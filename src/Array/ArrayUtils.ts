ff6335e1024d8d1907ff119137c6eecdb155de62
wioenena-q <brs_kprl09@outlook.com>
2021-06-26 20:03:22 +0300

    Moved 'range' method to extras.

--- src/Array/ArrayUtils.ts
+++ src/Array/ArrayUtils.ts
@@ -1,20 +1,3 @@
 export class ArrayUtils {
-    /**
-     *
-     * Creates a list of numbers between specified ranges.
-     * @static
-     * @param {number} [min=0]
-     * @param {number} [max=10]
-     * @returns {number[]}
-     * @memberof ArrayUtil
-     */
-    public static range(min: number = 0, max: number = 10): number[] {
-        const result: number[] = new Array(0);
 
-        for (let i = min; i <= max; i++) {
-            result.push(i);
-        }
-
-        return result;
-    }
 }
