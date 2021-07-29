854afba0fb337788ac60cce7f12c60086feebacf
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 22:18:12 +0300

    Added "strings#tag"

---
+++ src/String/tag.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * Returns the string with the specified tag.
+ * @param tag - The tag.
+ * @param content - Main string.
+ * @returns {string}
+ */
+export const tag = (tag: string, content: string) => `<${tag}>${content}</${tag}>`;
