59daf78c41e0e873e85ed574cfbb671e79c3ec94
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 23:14:22 +0300

    Added "use strict"

--- src/String/starts_with.ts
+++ src/String/starts_with.ts
@@ -1,8 +1,10 @@
 import { substring } from "./substring.ts";
 
 export const startsWith = (content: string, search: string, startPosition?: number) => {
+    "use strict";
+
     if (startPosition === undefined || startPosition > content.length)
         startPosition = 0;
 
-    return substring(content, startPosition, startPosition + search.length) == search;
+    return substring(content, startPosition, startPosition + search.length) === search;
 };
