8934aec50b2d72b1ceb7a18dd2a00fd5ef13ffd1
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 21:19:58 +0300

    Added JSDoc

--- src/String/deep_trim.ts
+++ src/String/deep_trim.ts
@@ -1,4 +1,12 @@
+"use strict";
+
 import { trim } from "./trim.ts";
 
-"use strict";
+/**
+ * Deletes all redundant spaces in the string.
+ * @param content - Main string.
+ * @returns {string}
+ * @example
+ * deepTrim("      Hello       friend.     "); // Hello friend.
+ */
 export const deepTrim = (content: string) => trim(content).replace(/\s{1,}/g, " ");
