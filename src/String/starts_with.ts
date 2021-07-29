e38653b17a3d0b7cc9f74f5a927f41270a148141
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 23:09:22 +0300

    Added "strings#startsWith"

---
+++ src/String/starts_with.ts
@@ -0,0 +1,8 @@
+import { substring } from "./substring.ts";
+
+export const startsWith = (content: string, search: string, startPosition?: number) => {
+    if (startPosition === undefined || startPosition > content.length)
+        startPosition = 0;
+
+    return substring(content, startPosition, startPosition + search.length) == search;
+};
---
+++ tests/strings/starts_with.test.ts
@@ -0,0 +1,16 @@
+import { Strings } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+
+const { startsWith } = Strings;
+
+Deno.test("startsWith", () => {
+    assertEquals(
+        startsWith("abcde", "a"),
+        "abcde".startsWith("a")
+    );
+
+    assertEquals(
+        startsWith("abcde", "d", 3),
+        "abcde".startsWith("d", 3),
+    );
+});
