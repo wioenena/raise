0cf6ff2e70f5f251025975274de244244e581423
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 22:47:30 +0300

    Added "strings#concat"

---
+++ src/String/concat.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * The concat() method concatenates the string arguments to the calling string and returns a new string.
+ * @param content - Main string.
+ * @param others - Other strings.
+ * @returns {string}
+ */
+export const concat = (content: string, ...others: string[]) => content + others.join("");
