78c5fbe9062404c419405aa1fe64956d904f5bee
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 21:22:57 +0300

    Added trimStart and trimEnd.

---
+++ src/String/trim_start.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * Deletes spaces at the start of the string.
+ * @param content - Main string.
+ * @returns {string}
+ * @example
+ * trimStart("   abc"); // abc
+ */
+export const trimStart = (content: string) => content.replace(/^\s+/, "");
