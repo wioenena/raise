6b67a7775c9e208d420949295c443da94b30c662
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 23:47:39 +0300

    Added "strings#includes"

---
+++ tests/strings/includes.test.ts
@@ -0,0 +1,16 @@
+import { Strings } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+
+const { includes } = Strings;
+
+Deno.test("includes", () => {
+    assertEquals(
+        includes("abcdef", "e"),
+        "abcdef".includes("e")
+    );
+
+    assertEquals(
+        includes("abcdef", "e", 2),
+        "abcdef".includes("e", 2)
+    );
+});
