7aec5d907ecdb30bbd9da553b971639f2e43a746
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 02:12:56 +0300

    Added JSDoc

--- src/String/last_index_of.ts
+++ src/String/last_index_of.ts
@@ -1,5 +1,12 @@
 import { indexOf } from "./index_of.ts";
 
+/**
+ * The lastIndexOf() method returns the index within the calling String object of the last occurrence of the specified value, searching backwards from fromIndex. Returns -1 if the value is not found.
+ * @param content - Main string.
+ * @param search - Search string.
+ * @param endPosition - End position.
+ * @returns {string}
+ */
 export const lastIndexOf = (content: string, search: string, endPosition?: number) => {
     "use strict";
 
