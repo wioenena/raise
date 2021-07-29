c90aa928650f99bcdd154f190c6463bcf6b841ad
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 02:14:41 +0300

    Code looks better now.

--- src/String/font_size.ts
+++ src/String/font_size.ts
@@ -2,6 +2,7 @@
 
 import { tag } from "./tag.ts";
 import { append } from "./append.ts";
+
 /**
  * The fontsize() method creates a <font> HTML element that causes a string to be displayed in the specified font size.
  * @param content - Main string.
