f43bbfb57eba3d90ef45b1e38431d9cc8232627d
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 21:21:22 +0300

    Changed regexp.

--- src/String/trim.ts
+++ src/String/trim.ts
@@ -1,6 +1,15 @@
+"use strict";
+
+/**
+ *
+ * @param content - Main string.
+ * @returns {string}
+ * @example
+ * trim("   abc   "); // abc
+ */
 export const trim = (content: string) => {
     "use strict";
 
     return content
-        .replace(/^\s+|\s+$/g, "");
+        .replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
 };
