b8156fbc7e07a0357eef77ee17630abc2b8ff2af
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 23:23:25 +0300

    Added "strings#fontColor"

---
+++ src/String/font_color.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ *
+ * @param content - Main string.
+ * @param color - Color
+ * @returns {string}
+ */
+export const fontColor = (content: string, color: string) => `<font color="${color}">${content}</font>`;
