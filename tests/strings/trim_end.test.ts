77a279131a5575d84ac4d3eef1c628e433adbbb3
wioenena-q <brs_kprl09@outlook.com>
2021-07-29 21:29:20 +0300

    Tested trimStart and trimEnd.

---
+++ tests/strings/trim_end.test.ts
@@ -0,0 +1,16 @@
+import { Strings } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+
+const { trimEnd } = Strings;
+
+Deno.test("trimEnd", () => {
+    assertEquals(
+        trimEnd("abc    "),
+        "abc"
+    );
+
+    assertEquals(
+        trimEnd("ab   c    "),
+        "ab   c"
+    );
+});
---
+++ tests/strings/trim_start.test.ts
@@ -0,0 +1,16 @@
+import { Strings } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+
+const { trimStart } = Strings;
+
+Deno.test("trimStart", () => {
+    assertEquals(
+        trimStart("   abc"),
+        "abc"
+    );
+
+    assertEquals(
+        trimStart("   ab   c"),
+        "ab   c"
+    );
+});
