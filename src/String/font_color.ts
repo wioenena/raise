7b6bc304178dc1359a380ba080c29c3de22ff416
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 02:13:24 +0300

    Code looks better now.

--- src/String/font_color.ts
+++ src/String/font_color.ts
@@ -1,9 +1,12 @@
 "use strict";
 
+import { tag } from "./tag.ts";
+import { append } from "./append.ts";
+
 /**
  * The fontcolor() method creates a <font> HTML element that causes a string to be displayed in the specified font color.
  * @param content - Main string.
  * @param color - Color
  * @returns {string}
  */
-export const fontColor = (content: string, color: string) => `<font color="${color}">${content}</font>`;
+export const fontColor = (content: string, color: string) => append(tag("font", content), ` color="${color}"`, 5);
--- src/String/font_size.ts
+++ src/String/font_size.ts
@@ -1,9 +1,11 @@
 "use strict";
 
+import { tag } from "./tag.ts";
+import { append } from "./append.ts";
 /**
  * The fontsize() method creates a <font> HTML element that causes a string to be displayed in the specified font size.
  * @param content - Main string.
  * @param size - Size
  * @returns {string}
  */
-export const fontSize = (content: string, size: number) => `<font size="${size}">${content}</font>`;
+export const fontSize = (content: string, size: number) => append(tag("font", content), ` size="${size}"`, 5);
