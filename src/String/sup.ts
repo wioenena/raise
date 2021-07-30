efb48da2ca251ae7ed9b4ebeb68590b1cbfd3fd9
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 09:48:33 +0300

    Added "strings#sup"

---
+++ src/String/sup.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import { tag } from "./tag.ts";
+
+/**
+ * The sup() method creates a <sup> HTML element that causes a string to be displayed as superscript.
+ * @param content - Main string.
+ * @returns {string}
+ */
+export const sup = (content: string) => tag("sup", content);
--- strings_mod.ts
+++ strings_mod.ts
@@ -27,3 +27,4 @@ export * from "./src/String/pad_end.ts";
 export * from "./src/String/slice.ts";
 export * from "./src/String/strike.ts";
 export * from "./src/String/small.ts";
+export * from "./src/String/sup.ts";
