b6c30fc52d95d724b8933da08b9d15eb0145dac9
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 20:33:49 +0300

    Added "Added strings#toUpperCase"

---
+++ src/String/to_upper_case.ts
@@ -0,0 +1,26 @@
+import { slice } from "./slice.ts";
+
+/**
+ * Same as "String.prototype.toUpperCase", only start and end indexes can be set.
+ * @param content - Main string.
+ * @param start - Start position.
+ * @param end - End position.
+ * @returns {string}
+ */
+export const toUpperCase = (content: string, start?: number, end?: number) => {
+    "use strict";
+
+    if (start === undefined && end === undefined)
+        return content.toUpperCase();
+
+    if (start === undefined || start > content.length || start < 0)
+        start = 0;
+
+    if (end === undefined || end > content.length || end < 0)
+        end = content.length;
+
+    if (start > end)
+        [start, end] = [end, start];
+
+    return slice(content, 0, start) + slice(content, start, end + 1).toUpperCase() + slice(content, end + 1, content.length);
+};
---
+++ tests/strings/to_upper_case.test.ts
@@ -0,0 +1,21 @@
+import { Strings } from "../../mod.ts";
+import { assertEquals } from "../../deps.ts";
+
+const { toUpperCase } = Strings;
+
+Deno.test("toUpperCase", () => {
+    assertEquals(
+        toUpperCase("abcdef", 2),
+        "abCDEF"
+    );
+
+    assertEquals(
+        toUpperCase("abcdef", 2, 4),
+        "abCDEf"
+    );
+
+    assertEquals(
+        toUpperCase("abcdef", 0, 4),
+        "ABCDEf"
+    );
+});
