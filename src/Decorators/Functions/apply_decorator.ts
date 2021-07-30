924091d329f59b8b7a6c84f9e87e4d38f435a893
wioenena-q <brs_kprl09@outlook.com>
2021-07-30 21:55:04 +0300

    Added JSDoc

--- src/Decorators/Functions/apply_decorator.ts
+++ src/Decorators/Functions/apply_decorator.ts
@@ -1,4 +1,11 @@
 // eslint-disable-next-line arrow-body-style
+
+/**
+ * Apply decorators for functions.
+ * @param base - Base function.
+ * @param decorator - Decorator function.
+ * @returns {unknown}
+ */
 export const applyDecorator = (base: Function, decorator: Function) => {
     return function wrapper<AType = unknown, RType = unknown>(...args: AType[]): RType {
         return decorator(base, ...args);
