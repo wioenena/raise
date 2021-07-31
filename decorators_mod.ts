c4b03606d62299b791eae6c032f8fd1228afdbdc
wioenena-q <brs_kprl09@outlook.com>
2021-07-31 19:04:17 +0300

    Quote rule set to single.

--- decorators_mod.ts
+++ decorators_mod.ts
@@ -1,4 +1,4 @@
-import { applyDecorator } from "./src/Decorators/Functions/apply_decorator.ts";
+import { applyDecorator } from './src/Decorators/Functions/apply_decorator.ts';
 
 export const Functions = {
     applyDecorator
