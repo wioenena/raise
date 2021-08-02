1453ba17c11e1e69e45256ea62b2c80b74ed6fc9
wioenena-q <brs_kprl09@outlook.com>
2021-08-02 21:02:19 +0300

    "require" exported for node modules.

--- deps.ts
+++ deps.ts
@@ -1,5 +1,9 @@
 import Mixed from 'https://deno.land/x/mixed@0.0.1/mod.ts';
 
+import { createRequire } from 'https://deno.land/std@0.103.0/node/module.ts';
+
 export { assertEquals } from 'https://deno.land/std@0.102.0/testing/asserts.ts';
+
+export const require = createRequire(import.meta.url);
 
 export { Mixed };
