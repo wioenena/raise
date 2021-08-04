64ee05963ec2d8d0ed136eb58f467df7dec7e0ad
wioenena-q <brs_kprl09@outlook.com>
2021-08-04 23:48:16 +0300

    Several methods have been deprecated.

--- strings_mod.ts
+++ strings_mod.ts
@@ -1,10 +1,7 @@
 export { default as append } from './src/String/append.ts';
 export { default as clone } from './src/String/clone.ts';
 export { default as deepTrim } from './src/String/deep_trim.ts';
-export { default as collectString } from './src/String/collect_string.ts';
-export { default as collectNumber } from './src/String/collect_number.ts';
 export { default as isString } from './src/String/is_string.ts';
 export { default as toString } from './src/String/to_string.ts';
-export { default as checkIsString } from './src/String/check_is_string.ts';
 export { default as endsWith } from "./src/String/ends_with.ts";
 
