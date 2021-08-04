efd8b9ea2b5aec30cca43b5047aa21498c957eaf
wioenena-q <brs_kprl09@outlook.com>
2021-08-05 00:00:14 +0300

    Several methods have been deprecated.

--- src/String/check_is_string.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import isString from './is_string.ts';
-
-export default function checkIsString(content: unknown) {
-    if (!isString(content))
-        throw new TypeError(`${content}'s type is not string. type of ${content} = ${typeof content}`);
-}
