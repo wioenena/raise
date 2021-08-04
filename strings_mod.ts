9ec61f9e55c276b7518392f05f98d57305e1f308
wioenena-q <brs_kprl09@outlook.com>
2021-08-04 23:52:13 +0300

    Added 'string#toLower'

--- strings_mod.ts
+++ strings_mod.ts
@@ -3,5 +3,6 @@ export { default as clone } from './src/String/clone.ts';
 export { default as deepTrim } from './src/String/deep_trim.ts';
 export { default as isString } from './src/String/is_string.ts';
 export { default as toString } from './src/String/to_string.ts';
-export { default as endsWith } from "./src/String/ends_with.ts";
-
+export { default as endsWith } from './src/String/ends_with.ts';
+export { default as toUpper } from './src/String/to_upper.ts';
+export { default as toLower } from './src/String/to_lower.ts';
